d0457fac596963dd6f0b2eb6bd455c0f943b1905
Yujing Zou <yujing.zou@mail.mcgill.ca>
2024-08-08 11:27:14 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Objective: a program to automatically turn large word document (~100 pages) 
+    with images into a compressed latex document zipped folder that can be 
+    directly uploaded to overleaf. 
+    I want all the origial images from the word doc to be saved and organized 
+    in the output folder. 
+
+Input: Large Word Doc to be converted to Latex 
+
+Output: Compressed folder to be directly uploaded to latex overleaf. 
+
+Author: Yujing Zou, August, 2024
+
+"""
+
